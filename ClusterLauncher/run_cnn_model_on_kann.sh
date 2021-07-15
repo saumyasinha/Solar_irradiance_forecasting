95de2552ad04f2d66011e7a8ae4712e38b09a5fa
Saumya Sinha <sasi1275@login11.rc.int.colorado.edu>
2021-07-15 14:37:31 -0600

    modify cluster launchers for ngboost

--- ClusterLauncher/run_cnn_model_on_kann.sh
+++ ClusterLauncher/run_cnn_model_on_kann.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # === 2. List of SBATCH arguements ===
-#SBATCH --job-name=multi_head_no_dense_dual_attention
+#SBATCH --job-name=ngboost_with_lag
 #SBATCH --partition=blanca-kann
 #SBATCH --account=blanca-kann
 #SBATCH --qos=blanca-kann
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=2-00:00:00
-#SBATCH --output=/pl/active/machinelearning/Solar_forecasting_project/job_outputs/multi_head_no_dense_dual_attention.%j.out
+#SBATCH --output=/pl/active/machinelearning/Solar_forecasting_project/job_outputs/ngboost_with_lag.%j.out
 # === 3. Purge and load needed modules ===
 module purge
 module load python/3.6.5
 # === 4. Additional commands needed to run a program ===
 echo "Set environment variables or create directories here!"
 # === 5. Running the program ===
-python -u ../script_run_CNN.py
+python -u ../script_run_ngboost.py
